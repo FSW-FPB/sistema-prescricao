69f57f121d6321e3e985bf2e35ce7ada97585a1d
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2024-11-09 15:17:27 -0300

    feat: Cradição de feature das bulas e refatoraçcao de codigo

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 
 # API de Prescrição Médica
 
-Este projeto define uma API para gerenciar prescrições médicas em uma clínica. Utiliza o framework Flask para criar endpoints HTTP que permitem a criação e recuperação de prescrições, além de interagir com o MongoDB para armazenamento dos dados.
+Este projeto define uma API para gerenciar prescrições médicas em uma clínica. Utiliza o framework Flask para criar endpoints HTTP que permitem a criação, atualização, recuperação e exclusão de prescrições, além de interagir com o MongoDB para armazenamento dos dados. A API também permite consultar medicamentos a partir de um arquivo CSV.
 
 ## Tecnologias Utilizadas
 - **Flask**: Framework web para criação da API.
 - **pymongo**: Biblioteca para interação com o banco de dados MongoDB.
 - **bson**: Biblioteca para manipulação de objetos do MongoDB, como o `ObjectId`.
+- **csv**: Para manipulação e leitura de arquivos CSV que contêm dados sobre medicamentos.
+- **datetime**: Para manipulação de data e hora.
 
 ## Configuração Inicial
 
@@ -16,7 +18,8 @@ Este projeto define uma API para gerenciar prescrições médicas em uma clínic
    pip install flask pymongo bson
    ```
 
-3. Execute o servidor Flask:
+3. Prepare o arquivo `medicamentos.csv` com os dados dos medicamentos na mesma pasta do projeto.
+4. Execute o servidor Flask:
    ```bash
    python app.py
    ```
@@ -29,7 +32,10 @@ A API estará disponível em `http://localhost:5000`.
 - **Descrição**: Cria uma nova prescrição médica com os dados fornecidos no corpo da requisição.
 - **Requisição**:
   - Formato: JSON
-  - Campos esperados: Variável, de acordo com as informações de uma prescrição.
+  - Campos esperados:
+    - `paciente_id`: ID do paciente.
+    - `medico_id`: ID do médico.
+    - `medicamentos`: Lista de medicamentos prescritos (com nome e outros dados).
 - **Resposta**:
   - Retorna um JSON com os dados da prescrição criada, incluindo o `id` gerado.
 
@@ -45,23 +51,109 @@ A API estará disponível em `http://localhost:5000`.
 - **Resposta**:
   - Retorna um JSON com os dados da prescrição solicitada ou um erro se o `id` não for encontrado.
 
+### `PATCH /prescricoes/<prescricao_id>`
+- **Descrição**: Atualiza parcialmente uma prescrição existente com base no `prescricao_id`.
+- **Requisição**:
+  - Formato: JSON
+  - Campos esperados:
+    - `paciente_id`: ID do paciente (opcional).
+    - `medicamentos`: Lista de medicamentos a ser atualizada (opcional).
+- **Resposta**:
+  - Retorna uma mensagem indicando se a atualização foi bem-sucedida ou se a prescrição não foi encontrada.
+
+### `DELETE /prescricoes/<prescricao_id>`
+- **Descrição**: Exclui uma prescrição existente com base no `prescricao_id`.
+- **Resposta**:
+  - Retorna uma mensagem indicando se a prescrição foi excluída com sucesso ou se não foi encontrada.
+
+### `GET /medicamentos`
+- **Descrição**: Recupera todos os medicamentos disponíveis no arquivo `medicamentos.csv`.
+- **Resposta**:
+  - Retorna uma lista de medicamentos em formato JSON.
+
+### `GET /medicamentos/busca`
+- **Descrição**: Busca medicamentos no arquivo `medicamentos.csv` com base em um nome fornecido na query string.
+- **Parâmetros**:
+  - `search`: Nome do medicamento ou parte dele.
+- **Resposta**:
+  - Retorna uma lista de medicamentos encontrados que correspondem ao nome fornecido.
+
 ## Exemplo de Uso
 
 ### Criar uma nova prescrição
 ```bash
- http://localhost:5000/prescricoes
+POST http://localhost:5000/prescricoes
+Content-Type: application/json
+{
+  "paciente_id": 1,
+  "medico_id": 1,
+  "doenca": "Dor de cabeça",
+  "medicamentos": [
+    {
+      "nome": "DORALGINA", 
+      "dosagem": "50mg", 
+      "frequencia": "a cada 24 horas"
+    },
+    {
+      "nome": "DORALGINA DIPCAF", 
+      "dosagem": "25mg", 
+      "frequencia": "a cada 16 horas"
+    }
+  ]
+}
 ```
 
 ### Recuperar todas as prescrições
 ```bash
- http://localhost:5000/prescricoes
+GET http://localhost:5000/prescricoes
 ```
 
 ### Recuperar uma prescrição específica
 ```bash
-http://localhost:5000/prescricoes/<id>
+GET http://localhost:5000/prescricoes/<id>
 ```
 
----
+### Atualizar uma prescrição existente
+```bash
+PATCH http://localhost:5000/prescricoes/<prescricao_id>
+Content-Type: application/json
+{
+  "paciente_id": 1,
+  "medico_id": 1,
+  "doenca": "Dor de cabeça",
+  "medicamentos": [
+    {
+      "nome": "PARACETAMOL", 
+      "dosagem": "500mg", 
+      "frequencia": "a cada 8 horas"
+    },
+    {
+      "nome": "DIPIRONA", 
+      "dosagem": "25ml", 
+      "frequencia": "a cada 12 horas"
+    }
+  ]
+}
+```
 
-Este README cobre as principais funcionalidades e exemplos de uso para a API de prescrição médica.
+### Excluir uma prescrição existente
+```bash
+DELETE http://localhost:5000/prescricoes/<prescricao_id>
+```
+
+### Buscar todos os medicamentos
+```bash
+GET http://localhost:5000/medicamentos
+```
+
+### Buscar medicamentos por nome
+```bash
+GET http://localhost:5000/medicamentos/busca?search=Paracetamol
+```
+
+## Membros do Projeto
+
+| Nome            | Foto                                                                                                                   | Perfil                                             |
+|-----------------|------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------|
+| **DaniloGnome** | <img src="https://avatars.githubusercontent.com/u/127751001?v=4" width="45" height="45" style="border-radius: 50%;" /> | [Perfil do GitHub](https://github.com/DaniloGnome) |
+| **Reed0ne**     | <img src="https://avatars.githubusercontent.com/u/115191418?v=4" width="45" height="45" style="border-radius: 50%;" /> | [Perfil do GitHub](https://github.com/Reed0ne)     |
--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import csv
+from datetime import datetime
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['clinica']
 
-# Endpoint para criar uma nova prescrição
 @app.route('/prescricoes', methods=['POST'])
 def criar_prescricao():
     dados = request.json
-    dados['id'] = str(ObjectId())  # Gera um ID único
+    medicamentos = dados.get('medicamentos', [])
+
+    # Verifica se os campos obrigatórios estão presentes
+    if not all(key in dados for key in ("paciente_id", "medico_id", "medicamentos")):
+        return jsonify({'error': 'Campos obrigatórios: paciente_id, medico_id e medicamentos'}), 400
+
+    # A data e hora atual no momento em que a requisição é recebida
+    dados['data_prescricao'] = datetime.utcnow().isoformat()  # Usando UTC para garantir uniformidade
+
+    # Adiciona informações detalhadas de cada medicamento
+    medicamentos_detalhados = []
+    for med in medicamentos:
+        nome_medicamento = med.get("nome")
+        if nome_medicamento:
+            # Busca o medicamento no arquivo CSV pelo nome
+            dados_medicamento = consultar_medicamento(nome_medicamento)
+            if "error" in dados_medicamento:
+                return jsonify({'error': dados_medicamento["error"]}), 404
+            # Agora o 'nome' vai dentro de 'informacoes_medicamento'
+            med['informacoes_medicamento'] = dados_medicamento
+            # Removendo o campo 'nome' da raiz do medicamento
+            del med['nome']
+            medicamentos_detalhados.append(med)
+
+    dados['medicamentos'] = medicamentos_detalhados
+
+    # Insere um ID único para a prescrição
+    dados['_id'] = ObjectId()  # Gera automaticamente um ID
     db.prescricoes.insert_one(dados)
 
-    # Convertendo o ID gerado em string para o retorno
-    dados['_id'] = dados['id']
+    # Converte o ID gerado em string para o retorno
+    dados['_id'] = str(dados['_id'])
     return jsonify(dados), 201
 
 # Endpoint para obter todas as prescrições
@@ -28,28 +56,150 @@ def obter_prescricoes():
 # Endpoint para obter uma prescrição específica
 @app.route('/prescricoes/<id>', methods=['GET'])
 def obter_prescricao(id):
-    prescricao = db.prescricoes.find_one({"id": id})
-    if prescricao:
-        prescricao['_id'] = str(prescricao['_id'])  # Converter ObjectId para string
-        return jsonify(prescricao), 200
-    return jsonify({'error': 'Prescrição não encontrada'}), 404
+    try:
+        # Converte o id recebido para ObjectId
+        prescricao = db.prescricoes.find_one({"_id": ObjectId(id)})
+        if prescricao:
+            prescricao['_id'] = str(prescricao['_id'])  # Converter ObjectId para string
+            return jsonify(prescricao), 200
+        return jsonify({'error': 'Prescrição não encontrada'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
-# Endpoint para atualizar uma prescrição existente
-@app.route('/prescricoes/<id>', methods=['PUT'])
-def atualizar_prescricao(id):
+# Endpoint para atualizar parcialmente uma prescrição
+@app.route('/prescricoes/<prescricao_id>', methods=['PATCH'])
+def atualizar_prescricao(prescricao_id):
     dados = request.json
-    resultado = db.prescricoes.update_one({"id": id}, {"$set": dados})
-    if resultado.matched_count > 0:
-        return jsonify({'message': 'Prescrição atualizada com sucesso'}), 200
-    return jsonify({'error': 'Prescrição não encontrada'}), 404
+
+    # Cria um dicionário com os campos que podem ser atualizados
+    campos_para_atualizar = {}
+
+    # Verifica se o campo 'paciente_id' foi fornecido
+    if 'paciente_id' in dados:
+        campos_para_atualizar['paciente_id'] = dados['paciente_id']
+
+    # Verifica se o campo 'medicamentos' foi fornecido
+    if 'medicamentos' in dados:
+        medicamentos_detalhados = []
+        for med in dados['medicamentos']:
+            nome_medicamento = med.get("nome")
+            if nome_medicamento:
+                # Consulta o medicamento no arquivo CSV
+                dados_medicamento = consultar_medicamento(nome_medicamento)
+                if "error" in dados_medicamento:
+                    return jsonify({'error': dados_medicamento["error"]}), 404
+                # Agora o 'nome' vai dentro de 'informacoes_medicamento'
+                med['informacoes_medicamento'] = dados_medicamento
+                # Removendo o campo 'nome' da raiz do medicamento
+                del med['nome']
+                medicamentos_detalhados.append(med)
+        campos_para_atualizar['medicamentos'] = medicamentos_detalhados
+
+    # Se nenhum campo válido for fornecido para atualização, retorna um erro
+    if not campos_para_atualizar:
+        return jsonify({'error': 'Nenhum campo para atualizar fornecido'}), 400
+
+    # Realiza a atualização no banco de dados
+    resultado = db.prescricoes.update_one(
+        {"_id": ObjectId(prescricao_id)},  # Filtro pela ID da prescrição
+        {"$set": campos_para_atualizar}  # Atualiza apenas os campos fornecidos
+    )
+
+    # Verifica se a prescrição foi encontrada e atualizada
+    if resultado.matched_count == 0:
+        return jsonify({'error': 'Prescrição não encontrada'}), 404
+
+    return jsonify({'message': 'Prescrição atualizada com sucesso'}), 200
 
 # Endpoint para deletar uma prescrição
-@app.route('/prescricoes/<id>', methods=['DELETE'])
-def deletar_prescricao(id):
-    resultado = db.prescricoes.delete_one({"id": id})
-    if resultado.deleted_count > 0:
-        return jsonify({'message': 'Prescrição deletada com sucesso'}), 200
-    return jsonify({'error': 'Prescrição não encontrada'}), 404
+@app.route('/prescricoes/<prescricao_id>', methods=['DELETE'])
+def deletar_prescricao(prescricao_id):
+    # Tenta excluir a prescrição com o ID fornecido
+    resultado = db.prescricoes.delete_one({"_id": ObjectId(prescricao_id)})
+
+    # Verifica se algum documento foi excluído
+    if resultado.deleted_count == 0:
+        return jsonify({'error': 'Prescrição não encontrada'}), 404
+
+    return jsonify({'message': 'Prescrição deletada com sucesso'}), 200
+
+
+@app.route('/medicamentos', methods=['GET'])
+def get_all_medicamentos():
+    medicamentos = []
+    try:
+        with open('medicamentos.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=';')
+            for row in reader:
+                nome_medicamento = row[1].strip().upper()
+                tipo_medicamento = row[7].strip() if row[7].strip() else "Tipo não disponível"
+                medicamentos.append({
+                    "nome": nome_medicamento,
+                    "tipo": tipo_medicamento
+                })
+        return jsonify(medicamentos), 200
+    except FileNotFoundError:
+        return jsonify({"error": "Arquivo medicamentos.csv não encontrado"}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Função para consultar medicamentos diretamente no arquivo CSV
+def consultar_medicamento(nome):
+    try:
+        with open('medicamentos.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=';')
+            for row in reader:
+                # O nome do medicamento está na segunda coluna (index 1)
+                nome_medicamento = row[1].strip().upper()  # Remove espaços e ignora case sensitivity
+                # O tipo do medicamento está na oitava coluna (index 7)
+                tipo_medicamento = row[7].strip()
+
+                # Comparando o nome fornecido com o nome do medicamento no CSV
+                if nome_medicamento == nome.upper():
+                    return {
+                        "nome": nome_medicamento,
+                        "tipo": tipo_medicamento if tipo_medicamento else "Tipo não disponível"
+                    }
+        return {"error": "Medicamento não encontrado"}
+    except FileNotFoundError:
+        return {"error": "Arquivo medicamentos.csv não encontrado"}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.route('/medicamentos/busca', methods=['GET'])
+def buscar_medicamento_por_nome():
+    nome = request.args.get('search')  # Obtém o parâmetro 'search' da query string
+
+    if not nome:
+        return jsonify({'error': 'Parâmetro "search" é obrigatório'}), 400
+
+    try:
+        medicamentos_encontrados = []
+        with open('medicamentos.csv', mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=';')
+            for row in reader:
+                nome_medicamento = row[1].strip().upper()  # Nome do medicamento
+                tipo_medicamento = row[7].strip()  # Tipo do medicamento
+
+                # Verifica se o nome buscado é um fragmento do nome do medicamento
+                if nome.upper() in nome_medicamento:
+                    medicamentos_encontrados.append({
+                        "nome": nome_medicamento,
+                        "tipo": tipo_medicamento if tipo_medicamento else "Tipo não disponível"
+                    })
+
+        # Se não encontrar nenhum medicamento, retorna erro
+        if not medicamentos_encontrados:
+            return jsonify({'error': 'Nenhum medicamento encontrado com esse nome'}), 404
+
+        return jsonify(medicamentos_encontrados), 200
+
+    except FileNotFoundError:
+        return jsonify({"error": "Arquivo medicamentos.csv não encontrado"}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
